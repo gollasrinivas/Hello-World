4a93c0fc9196a06445cb4d8371edbb572bbe0b20
Srinivas Golla <gsrinivas0000@gmail.com>
2022-07-03 15:28:30 +0530

    test sprint boot

---
+++ src/main/java/com/helloworld/helloworld/HelloworldController.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloworldController {
+    
+    @GetMapping("/")
+    public String helloworld(){
+        return "HELLO WORLD";
+    }
+}
